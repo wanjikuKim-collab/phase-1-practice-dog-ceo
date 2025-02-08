1af1a7bc331ae94998c7d5fc21235be031082228
Faith Kimani <wanjikukimani24@gmail.com>
2025-02-08 19:46:48 +0000

    style: Add style sheet and styling for the rendered dog images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const dogImageContainer = document.getElementById('dog-image-container');
+const dogBreeds = document.getElementById() 
+
 function fetchRandomDogs(){
     fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(res => res.json())
@@ -9,15 +11,21 @@ function fetchRandomDogs(){
         })
 }
 
+function fetchDogBreed(){
+
+}
+
 function displayDogImages(dogs){
     dogs.map(dog=>{
         console.log(dog);
         let img = document.createElement('img');
+        img.className = 'dog-image';
         img.src = dog;
         img.alt = 'A random dog';
-        img.style.width = " 300px";
-        img.style.height ='300px';
-        img.style.objectFit = 'cover'
+        // img.style.width = " 300px";
+        // img.style.height ='300px';
+        // img.style.objectFit = 'cover'
+        // img.style.padding = '20px'
 
         dogImageContainer.append(img);
 
