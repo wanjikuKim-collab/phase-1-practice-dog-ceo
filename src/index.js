e32ed1c5495e5bf6b0793455408854b69cbe2ec7
Faith Kimani <wanjikukimani24@gmail.com>
2025-02-08 19:33:56 +0000

    feat: displays dog images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const dogImageContainer = document.getElementById('dog-image-container');
 function fetchRandomDogs(){
     fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(res => res.json())
@@ -9,7 +10,18 @@ function fetchRandomDogs(){
 }
 
 function displayDogImages(dogs){
-    dogs.map(dog=> console.log(dog))
+    dogs.map(dog=>{
+        console.log(dog);
+        let img = document.createElement('img');
+        img.src = dog;
+        img.alt = 'A random dog';
+        img.style.width = " 300px";
+        img.style.height ='300px';
+        img.style.objectFit = 'cover'
+
+        dogImageContainer.append(img);
+
+    })
 }
 
 fetchRandomDogs();
