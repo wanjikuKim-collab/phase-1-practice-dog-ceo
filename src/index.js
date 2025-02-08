dfc79e100e3ffceb0a2138104220c0bcb292e200
Faith Kimani <wanjikukimani24@gmail.com>
2025-02-08 19:12:55 +0000

    feat: fetch dog images and displays on dev tools

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('%c HI', 'color: firebrick')
+function fetchRandomDogs(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+    .then(dogs=>displayDogImages(dogs.message))
+    .catch(error =>{
+         alert("Error fetching dog images");
+         console.log(error.message)
+        })
+}
+
+function displayDogImages(dogs){
+    dogs.map(dog=> console.log(dog))
+}
+
+fetchRandomDogs();
